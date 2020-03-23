14105548a6fe2750cf386e8aaf7f9d1aee3792b2
neha-vasu <nv851@nyu.edu>
2020-03-23 01:00:09 -0400

    hii

---
+++ overall/midtermUpdated/hover.js
@@ -0,0 +1,18 @@
+  
+
+      
+        function changeText(text)
+        {
+            var display = document.getElementById('text-display');
+            display.innerHTML = "";
+            display.innerHTML = text;
+        }
+              
+        function defaultText(text)
+        {
+            var display = document.getElementById('text-display');
+            display.innerHTML = "";
+            display.innerHTML = text;
+        }
+          
+    
